5d5fdf0448df6ec00e73c0a459ba4b7562d46257
chen <c252156290@gmail.com>
2024-10-10 12:11:43 +0800

    update app-install.sh

--- app-install.sh
+++ app-install.sh
@@ -7,8 +7,18 @@ sudo apt install -y \
     zsh \
     gh \
     fzf \
-    fd-find
+    ripgrep  # https://github.com/BurntSushi/ripgrep
 
+# install zoxide https://github.com/ajeetdsouza/zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# install fd-find
+sudo apt install -y \
+    fd-find
+if [ ! -d ~/.local/bin ]; then
+    mkdir -p ~/.local/bin
+fi
+ln -s $(which fdfind) ~/.local/bin/fd
 
 # install neovim version vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
